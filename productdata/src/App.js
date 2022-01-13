56d39b3a47d08b97bf41db127a2e51dbae3af15a
DelfinMong <delfdevs@gmail.com>
2022-01-13 02:32:50 -0500

    component

---
+++ productdata/src/App.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+import Home from "./Home"
+import Products from "./Products"
+
+function App() {    
+    return (
+        <div>
+            <nav>
+                <a>Home</a>
+                <a>Products</a>
+            </nav>
+            
+        </div>
+    )
+}
+
+export default App
