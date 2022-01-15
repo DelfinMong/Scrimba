321484c0454c2e3df368728596d0312da24a5393
DelfinMong <delfdevs@gmail.com>
2022-01-14 23:35:03 -0500

    scrimba

--- productdata/productdata/src/App.js
+++ productdata/productdata/src/App.js
@@ -1,10 +1,14 @@
 import React from "react"
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Link } from 'react-router-dom';
+import Home from './Home';
+import About from './About';
 
 function App() {    
     return (
         <div>
             <h1>HELLO WORLD</h1>
+            <Link to='/'>Home</Link>
+            <Link to='/about'>About</Link>
             <Switch>
                 <Route exact path="/"></Route>
                 <Route exact path="/"></Route>
