69df975ed8cca7a890ea6402bdfe3dcdfa2c1437
DelfinMong <delfdevs@gmail.com>
2022-02-05 23:40:21 -0500

    courandteac

--- teacher/src/components/App.js
+++ teacher/src/components/App.js
@@ -4,6 +4,8 @@ import { Route, BrowserRouter } from 'react-router-dom'
 import Home from './Home'
 import About from './About'
 import Header from './Header'
+import Teacher from './Teachers'
+import Course from './Courses'
 
 const App = () => (
   <BrowserRouter>
@@ -11,6 +13,8 @@ const App = () => (
       <Header />
       <Route exact path='/' component={Home} />
       <Route path='/about' component={About} />
+      <Route path='/teachers' component={Teacher} />
+      <Route path='/courses' component={Course} />
      </div>
   </BrowserRouter>
  
