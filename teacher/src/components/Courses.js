c49662e5bc89167fadc0100369c0cde2866ae51e
DelfinMong <delfdevs@gmail.com>
2022-02-07 19:49:48 -0500

    match

--- teacher/src/components/Courses.js
+++ teacher/src/components/Courses.js
@@ -5,19 +5,22 @@ import HTML from './courses/HTML'
 import CSS from './courses/CSS'
 import JavaScript from './courses/JavaScript'
 
-const Courses = () => (
+const Courses = ({match}) => (
   <div className="main-content courses">
     <div className="course-header group">
       <h2>Courses</h2> 
       <ul className="course-nav">
-        <NavLink to='/courses/html'>HTML</NavLink>
-        <NavLink to='/courses/css'>CSS</NavLink>
-        <NavLink to='/courses/javascript'>JavaScript</NavLink>
+        <NavLink to={`${match.url}/html`}>HTML</NavLink>
+        {/* <NavLink to={`${match.url}/css`} >CSS</NavLink> */}
+        <NavLink to='/courses/css' >CSS</NavLink>
+        <NavLink to={`${match.url}/javascript`}>JavaScript</NavLink>
       </ul>
-      <Redirect to='/courses/html' />
-      <Route path='/courses/html' component={HTML} />
+     
+      <Route exact path={match.path} render={ () => <Redirect to={`${match.url}/html`} />} />
+      <Route path={`${match.path}/html`} component={HTML} />
+      {/* <Route path={`${match.path}/css`} component={CSS} /> */}
       <Route path='/courses/css' component={CSS} />
-      <Route path='/courses/javascript' component={JavaScript} />
+      <Route path={`${match.path}/javascript`} component={JavaScript} />
     </div>
     
     {/* Write routes here... */}
@@ -28,3 +31,7 @@ export default Courses;
 
 // <Redirect to='/' /> requires a url to redirect to.
 // <Redirect to='/' /> will redirect to the home component.
+
+// we can change courses dynamatically without breaking the code.
+// path - (string) The path pattern used to match. Useful for building nested <Route>s
+// url - (string) The matched portion of the URL. Useful for building nested <Link>s
