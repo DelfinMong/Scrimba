043ad5c105e19f74be3eb52ef3c576495550a369
DelfinMong <delfdevs@gmail.com>
2022-02-09 15:51:23 -0500

    notfound

--- teacher/src/components/App.js
+++ teacher/src/components/App.js
@@ -1,23 +1,27 @@
 import React from 'react';
-import { Route, BrowserRouter } from 'react-router-dom'
+import { Route, BrowserRouter,Switch } from 'react-router-dom'
 
 import Home from './Home'
 import About from './About'
 import Header from './Header'
 import Teacher from './Teachers'
 import Course from './Courses'
+import NotFound from './NotFound';
 
 
 const App = () => (
   <BrowserRouter>
      <div className="container">
       <Header />
-      <Route exact path='/' component={Home} />
-      <Route path='/about' component={About} />
-      {/* <Route path='/about' render = { () => <About title='About'/>} /> */}
-      <Route path='/teachers' component={Teacher} />
-      <Route path='/courses' component={Course} />
-     </div>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path='/about' component={About} />
+          {/* <Route path='/about' render = { () => <About title='About'/>} /> */}
+          <Route path='/teachers' component={Teacher} />
+          <Route path='/courses' component={Course} />
+          <Route component={NotFound} />
+        </Switch>
+    </div>
   </BrowserRouter>
  
 );
